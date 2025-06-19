cf25f4881e6c861f408f831323ff12c2e26b87b6
vran <vran_dev@foxmail.com>
2025-06-19 22:29:24 +0800

    fix: property value field type crash

--- src/utils/getPropertyValues.ts
+++ src/utils/getPropertyValues.ts
@@ -6,7 +6,7 @@ import { PropertyType } from "./PropertyType";
 export function getPropertyValues(app: App, property: string): any[] {
     const values = app.metadataCache.getFrontmatterPropertyValuesForKey(property);
     if (!Objects.isNullOrUndefined(values) || values.length == 0) {
-        const propType = getPropertyTypeByName(this.plugin.app, property);
+        const propType = getPropertyTypeByName(app, property);
         if (propType == PropertyType.checkbox) {
             return ["true", "false"];
         }
