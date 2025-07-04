b0d22275cca322098e6bf88e00bf4a48c872c065
Moy <moyfoxther@gmail.com>
2025-07-04 14:38:31 +0800

    feat: add an api to directly submit form

--- plugin/src/api/FormFlowApi.ts
+++ plugin/src/api/FormFlowApi.ts
@@ -1,5 +1,6 @@
 import { App, Notice, TFile } from "obsidian";
 import { FormService } from "src/service/FormService";
+import { FormConfig } from "src/model/FormConfig";
 
 export class FormFlowApi {
 
@@ -12,6 +13,17 @@ export class FormFlowApi {
         const file = this.app.vault.getAbstractFileByPath(filePath);
         if (file instanceof TFile) {
             await formService.open(file, this.app);
+        } else {
+            new Notice(`Form File not found: ${filePath}`);
+        }
+    }
+	
+    async directlySubmitForm(filePath: string): Promise<void> {
+        const formService = new FormService();
+        const file = this.app.vault.getAbstractFileByPath(filePath);
+        if (file instanceof TFile) {
+            const form = await this.app.vault.readJson(file.path) as FormConfig;
+            await formService.submitDirectly(form, this.app);
         } else {
             new Notice(`Form File not found: ${filePath}`);
         }
