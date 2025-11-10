ae1041ee2c763d7f46604ab46b1cbba0c4b0f00b
RavenHogWarts <58649975+RavenHogWarts@users.noreply.github.com>
2025-11-10 23:55:47 +0800

    Refactor MoveFileFormAction to extend FileBaseFormAction
    Refactor MoveFileFormAction to extend FileBaseFormAction and replace previous flags with explicit targetFileType and moveTargetFolder properties, clarifying intent and matching other file-related actions.

--- plugin/src/model/action/MoveFileFormAction.ts
+++ plugin/src/model/action/MoveFileFormAction.ts
@@ -1,18 +1,19 @@
 import { FormActionType } from "../enums/FormActionType";
-import { BaseFormAction } from "./BaseFormAction";
+import { TargetFileType } from "../enums/TargetFileType";
+import { FileBaseFormAction } from "./FileBaseFormAction";
 
-export class MoveFileFormAction extends BaseFormAction {
+export class MoveFileFormAction extends FileBaseFormAction {
     type: FormActionType.MOVE_FILE;
-    isCurrentFile: boolean = true;
-    file: string;
-    targetFolder: string;
+    
+    targetFileType: TargetFileType;
+    
+    moveTargetFolder: string;
 
     constructor(partial?: Partial<MoveFileFormAction>) {
         super(partial);
         this.type = FormActionType.MOVE_FILE;
-        this.isCurrentFile = true;
-        this.file = "";
-        this.targetFolder = "";
+        this.targetFileType = TargetFileType.CURRENT_FILE;
+        this.moveTargetFolder = "";
         Object.assign(this, partial);
     }
 }
